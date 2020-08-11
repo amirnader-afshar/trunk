46d923e66667442e41f15e222706b5b922995f2d
naderenator <naderenator@gmail.com>
2020-08-11 15:44:03 +0430

    comment screen

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -17,6 +17,7 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
+
   String title = "";
   String img_url = "";
   String content = "";
@@ -43,29 +44,17 @@ class _ProductPageState extends State<ProductPage> {
 
   @override
   Widget build(BuildContext context) {
-    String titel = "";
-    titel = widget.product_instanse.title;
-    titel = titel.length > 30 ? titel.substring(0, 30) + ' .... ' : titel;
+
 
     return MaterialApp(
       home:  Scaffold(
         appBar: AppBar(
-
           backgroundColor: Colors.red,
-          title: Text(titel),
+          title: Text(title),
         ),
         body:
           (
-              !titel.isEmpty?SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Image(image: NetworkImage(img_url),)
-                    ,Text(title)
-                    ,Text(content)
-                  ],
-                ),
-              )
-                  :Container(child: Center(child: CircularProgressIndicator(),),)
+              _children(tab_index)
           ),
         bottomNavigationBar: BottomNavigationBar(items: [
           BottomNavigationBarItem(icon: Icon(Icons.title),title: Text("توضیحات")),
@@ -78,8 +67,73 @@ class _ProductPageState extends State<ProductPage> {
             tab_index=index;
           });
         }
-        ,currentIndex: tab_index,),
+        ,currentIndex: tab_index,backgroundColor: Colors.deepOrangeAccent,fixedColor: Colors.white70,
+        unselectedItemColor: Colors.black,),
       ),
     );
   }
+
+  Widget _children(int tab_index) {
+
+    List<Widget> page_screen=[];
+
+    page_screen.add(_tozihat_screen());
+    page_screen.add(_comment_Screen());
+    page_screen.add(_gallery_Screen());
+
+    return page_screen[tab_index];
+
+  }
+
+  Widget _tozihat_screen(){
+    String titel = "";
+    titel = widget.product_instanse.title;
+    titel = titel.length > 30 ? titel.substring(0, 30) + ' .... ' : titel;
+    return     !titel.isEmpty?SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Image(image: NetworkImage(img_url),)
+          ,Text(title)
+          ,Text(content)
+        ],
+      ),
+    )
+        :Container(child: Center(child: CircularProgressIndicator(),),);
+  }
+
+
+ Widget _comment_Screen(){
+
+    return SingleChildScrollView(child: Container(child:  Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(decoration: InputDecoration(prefixIcon: Icon(Icons.person,color: Colors.red,),labelText: "نام و نام خانوادگی",
+              border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5)))
+          )
+            ,),
+        ),      Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(decoration: InputDecoration(prefixIcon: Icon(Icons.email,color: Colors.red,),labelText: "e-mail",
+              border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5)))
+          )
+            ,),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(keyboardType: TextInputType.multiline,maxLines: null,minLines: 5,decoration: InputDecoration(prefixIcon: Icon(Icons.comment,color: Colors.red,),labelText: "نظر",
+              border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5)))
+          )
+            ,),
+        ),
+        ButtonTheme(height: 50,minWidth: MediaQuery.of(context).size.width,child:RaisedButton(onPressed: (){},child: Text("ثبت",style: TextStyle(color: Colors.white70),),color: Colors.red,)
+          ,)
+      ],),
+    )));
+ }
+ Widget _gallery_Screen(){
+
+    return Container(child: Center(child: Text("gallery"),),);
+ }
 }
