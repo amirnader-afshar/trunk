dd3b36d69211fd99c9451c70f6ad02ba2ba36b04
naderenator <naderenator@gmail.com>
2020-08-24 15:22:40 +0430

    floatin button

--- lib/screens/Comments.dart
+++ lib/screens/Comments.dart
@@ -17,6 +17,7 @@ class Comments extends StatefulWidget {
 class _CommentsView extends State<Comments> {
 
   int page =1;
+  int more_data=1;
 
   ScrollController _scrollController = new ScrollController();
   void initState(){
@@ -35,44 +36,60 @@ class _CommentsView extends State<Comments> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      controller: _scrollController,
-      itemBuilder: (BuildContext contex, index) {
-        return comment_row(index);
-      },
-      itemCount: widget.comment_list.length,
-    );
+    int itemCount = more_data==1?widget.comment_list.length+1:widget.comment_list.length;
+    return widget.comment_list.length>0 ?
+        Stack(children: [ListView.builder(
+          controller: _scrollController,
+          itemBuilder: (BuildContext contex, index) {
+            return comment_row(index);
+          },
+          itemCount: itemCount,
+        ),Positioned(bottom: 25,right: 25,child:
+        FloatingActionButton(onPressed: (){},child: Icon(Icons.add),backgroundColor: Colors.red,),)],)
+        :Container(child: Center(child: CircularProgressIndicator(),),);
   }
 
   Widget comment_row(int index) {
     double w = MediaQuery.of(context).size.width-30;
-    return Container( decoration: BoxDecoration(border:Border.all(color: Colors.grey[400],width: 1), ),
-      margin: EdgeInsets.all(15),
-      child: Column(
-        children: <Widget>[
-          Container(padding: EdgeInsets.all(10),
-            child: Row(children: [
-              Expanded(child: Text(" ارسالی از :"+widget.comment_list[index]['name']),
-              )
-              ,Expanded(child: Text("تاریخ ثبت : 1399/06/01",textAlign: TextAlign.right,))
-            ],),
+    if (index==widget.comment_list.length){
+      return Container(height: 100,width: w, child: Center(child: CircularProgressIndicator(),),);
+    }
+    else{
+      return Container( decoration: BoxDecoration(border:Border.all(color: Colors.grey[400],width: 1), ),
+        margin: EdgeInsets.all(15),
+        child: Column(
+          children: <Widget>[
+            Container(padding: EdgeInsets.all(10),
+              child: Row(children: [
+                Expanded(child: Text(" ارسالی از :"+widget.comment_list[index]['name']),
+                )
+                ,Expanded(child: Text("تاریخ ثبت : 1399/06/01",textAlign: TextAlign.right,))
+              ],),
               width: w,color: Colors.grey[200],)
-         ,Container(width: w,padding: EdgeInsets.all(10), child: Text(widget.comment_list[index]['content']),color: Colors.white,)
-        ],
-      ),
+            ,Container(width: w,padding: EdgeInsets.all(10), child: Text(widget.comment_list[index]['content']),color: Colors.white,)
+          ],
+        ),
 
-    );
+      );
+    }
   }
 
   void _loadData(Product product_instanse){
     http
-        .get(AppData.App_URL + 'comment/' + product_instanse.id+'&page='+page.toString(),
+        .get(AppData.App_URL + 'comment/' + product_instanse.id+'&'+page.toString(),
         headers: AppData.userHeader)
         .then((response) => {
       if (response.statusCode == 200)
         {
           setState(() {
-            widget.comment_list = convert.jsonDecode(response.body);
+            if (response.body=="[]")
+              {
+                more_data = 0;
+              }
+            else {
+              widget.comment_list =
+                  widget.comment_list + convert.jsonDecode(response.body);
+            }
           })
           ,
           page=page+1
